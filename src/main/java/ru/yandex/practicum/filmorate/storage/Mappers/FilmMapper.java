edbcbd1a071bf458efe5870d0a442c8bdd520c3b
Alexandr Nazyan <alexander.nazyan@yandex.ru>
2023-02-11 16:39:07 +0300

    убрал закомиченный код

--- src/main/java/ru/yandex/practicum/filmorate/storage/Mappers/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Mappers/FilmMapper.java
@@ -12,12 +12,5 @@ public class FilmMapper implements RowMapper<Film> {
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Film(rs.getInt("ID"), rs.getString("NAME"), rs.getString("DESCRIPTION"),
                 rs.getDate("RELEASE_DATE").toLocalDate(), rs.getInt("DURATION"));
-//              return  Film.builder()
-//                .id(rs.getInt("ID"))
-//                .name()
-//                .description(rs.getString("DESCRIPTION"))
-//                .releaseDate(rs.getDate("RELEASE_DATE").toLocalDate())
-//                .duration(rs.getInt("DURATION"))
-//                .build();
     }
 }
